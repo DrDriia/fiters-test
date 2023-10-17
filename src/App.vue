94dfddaf50eac3797395d5f409b7a51bbe83c08b
Daria <dd.chemloul@gmail.com>
2023-10-17 21:28:01 +0200

    First commit

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,17 @@
 <template>
   <img alt="Vue logo" src="./assets/logo.png">
   <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <PieChart />
 </template>
 
 <script>
 import HelloWorld from './components/HelloWorld.vue'
+import PieChart from './components/PieChart.vue'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    HelloWorld, PieChart
   }
 }
 </script>
---
+++ src/components/PieChart.vue
@@ -0,0 +1,27 @@
+<template>
+    <canvas id="myChart"></canvas>
+</template>
+  
+<script>
+    import Chart from 'chart.js/auto'
+
+    export default {
+        name: 'PieChart',
+        mounted(){
+            console.log('Mounted');
+
+            const ctx = document.getElementById('myChart');
+
+            const myChart = new Chart(ctx, {
+                type: 'pie',
+                data: {
+                    labels: ['Red', 'Blue', 'Yellow', 'Green', 'Purple', 'Orange'],
+                    datasets: [{
+                    data: [12, 19, 3, 5, 2, 3],
+                    borderWidth: 1
+                    }]
+                }
+                });
+        }
+    }
+</script>
