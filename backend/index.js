96e72874e9eb7e31886a62334f00918e55969aa9
Daria <dd.chemloul@gmail.com>
2023-10-20 13:06:22 +0200

    Endpoints done

--- backend/index.js
+++ backend/index.js
@@ -1,29 +1,11 @@
 const express = require('express');
 const cors = require('cors');
-const Data = require('./api/data');
+const Router = require('./api/endpoints');
 
 const app = express();
 app.use(cors());
+app.use('/', Router);
 
-const employe = "employee_fitness"
-const performance = "evolution_progression_sportive_6mois"
-
-const d = new Data('../database/' + employe + '.csv');
-const dataEmploye = d.getData();
-
-app.get('/employe', (req,res) => {
-    res.status(200).json(dataEmploye);
-});
-
-app.get('/employe/:id', (req,res) => {
-    const employe = dataEmploye.find(employe => employe.id === req.params.id);
-    res.status(200).send(employe);
-});
-
-/*app.get('/index', (req,res) => {
-    res.send("Here's the index");
-});*/
-
-app.listen(8080, () => {
+app.listen(3000, () => {
     console.log("Serveur à l'écoute");
 });
--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,17 +1,30 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
-  <PieChart />
+  <div>
+    Hello ?
+    <select id="selectCat">
+      <option value="">Global</option>
+      <option value="genre">Genre</option>
+      <option value="categorie_socio_professionnelle">Poste</option>
+      <option value="id">Individuel</option>
+    </select>
+    <PieChart />
+    <BarParticipate />
+    <LinePerf />
+    <ScatterPerfPart />
+  </div>
 </template>
 
 <script>
 import HelloWorld from './components/HelloWorld.vue'
-import PieChart from './components/PieChart.vue'
+import PieChart from './components/PieCategory.vue'
+import BarParticipate from './components/BarParticipate.vue'
+import LinePerf from './components/LinePerf.vue'
+import ScatterPerfPart from './components/ScatterPerfPart.vue'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld, PieChart
+    HelloWorld, PieChart, BarParticipate, LinePerf, ScatterPerfPart
   }
 }
 </script>
@@ -24,5 +37,6 @@ export default {
   text-align: center;
   color: #2c3e50;
   margin-top: 60px;
+  display:flex
 }
 </style>
